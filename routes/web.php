964ea998ba8864c789df40192a9717ef0fb93022
isal <memphissall580.com>
2025-09-06 11:06:21 +0700

    latihan_laravel

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'hello word dari tadi';
+});
+
+Route::get('/nama', function () {
+    return 'nama saya adalah isal';
+});
